6d4fedae88d6f816aef3bc5723b2e6686ef53dec
Pablo Corral <pcorralr3@gmail.com>
2025-10-01 09:41:26 +0200

    e12

---
+++ PYTHON/e12.py
@@ -0,0 +1,20 @@
+'''
+Ejercicio 12
+Una panadería vende barras de pan a 3.49€ cada una. El pan
+que no es el día tiene un descuento del 60%. Escribir un
+programa que comience leyendo el número de barras vendidas
+que no son del día. Después el programa debe mostrar el
+precio habitual de una barra de pan, el descuento que se le
+hace por no ser fresca y el coste final total.
+'''
+descuento = 0.60
+precioBarra = 3.49
+precioDescuento = precioBarra-precioBarra*descuento
+
+panes = int(input("Dame el número de barras de pan: \n"))
+
+costeTotal = round(panes*precioDescuento,2)
+totalDescontado = round(precioBarra*panes-costeTotal,2)
+totalSinDescuento = round(precioBarra*panes,2)
+
+print(f"Número de barras: {panes}\nCoste total: {costeTotal}€\nDescuento obtenido: {totalDescontado}€")
