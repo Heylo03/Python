59f98a104560baf88f39efbbcbeb5110b88d1c8b
Pablo Corral <pcorralr3@gmail.com>
2025-10-07 13:51:26 +0200

    e4

---
+++ PYTHON/Relacion2/e4.py
@@ -0,0 +1,12 @@
+'''
+Los teléfonos de una empresa tienen el siguiente formato
+prefijo-número-extension donde el prefijo es el código del país +34, y la
+extensión tiene dos dígitos (por ejemplo +34-913724710-56). Escribir un
+programa que pregunte por un número de teléfono con este formato y muestre por
+pantalla el número de teléfono sin el prefijo y la extensión.
+'''
+tel = input("Dame el numero de telefono \n")
+tel = tel.split("-")
+print(f"PREFIJO: {tel[0]}\n")
+print(f"NUMERO: {tel[1]}\n")
+print(f"EXTENSION: {tel[2]}\n")
