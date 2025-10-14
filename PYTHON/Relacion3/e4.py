65562d1ed1a86dbaf373be3fc4db7263c56467f0
Pablo Corral <pcorralr3@gmail.com>
2025-10-14 13:34:55 +0200

    e4

---
+++ PYTHON/Relacion3/e4.py
@@ -0,0 +1,10 @@
+'''
+Ejercicio 4
+Escribir un programa que pida al usuario un número entero y muestre por pantalla si
+es par o impar
+'''
+n = int(input("DAME UN NUMERO: \n"))
+if(n % 2 == 0):
+    print("NUMERO PAR")
+else: 
+    print("NUMERO IMPAR")
