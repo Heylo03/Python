a6731d9a7c550b1c251da3d739860d15cc7e3e23
Pablo Corral <pcorralr3@gmail.com>
2025-10-14 13:26:33 +0200

    e1

---
+++ PYTHON/Relacion3/e1.py
@@ -0,0 +1,10 @@
+'''
+Ejercicio 1
+Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es
+mayor de edad o no.
+'''
+edad = int(input("Dame tu edad: \n"))
+if(edad>=18):
+    print("ERES MAYOR DE EDAD")
+else:
+    print("ERES MENOR DE EDAD")
