a079426496bf0d112b316bf222f9a73c5a2d5f6f
Pablo Corral <pcorralr3@gmail.com>
2025-11-04 14:18:24 +0100

    e12

---
+++ PYTHON/Relacion4/e12.py
@@ -0,0 +1,18 @@
+'''
+Escribir un programa en el que se pregunte al usuario por una frase y una letra, y
+muestre por pantalla el número de veces que aparece la letra en la frase.
+'''
+
+frase = "Escribir un programa en el que se pregunte al usuario por una frase y una letra, y \
+muestre por pantalla el número de veces que aparece la letra en la frase."
+
+letra = input("DAME UNA LETRA: \n").lower()
+frase.lower()
+c = 0
+#print(frase.find(letra))
+for i in range(0,len(frase)):
+    if frase[i] == letra:
+        c += 1
+print(c)
+
+
