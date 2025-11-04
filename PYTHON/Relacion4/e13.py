02f36f89e4d64cd07e4674d0dbfa62692ecf2d22
Pablo Corral <pcorralr3@gmail.com>
2025-11-04 14:25:56 +0100

    e13

---
+++ PYTHON/Relacion4/e13.py
@@ -0,0 +1,12 @@
+'''
+Escribir un programa que muestre el eco de todo lo que el usuario introduzca hasta
+que el usuario escriba “salir” que terminará.
+'''
+
+salida = False
+while(salida == False):
+    str = input("Escribe: \n")
+    if str == "salida":
+        salida = True
+    else: 
+        print(str)
