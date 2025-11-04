e22c19e46279f010cdba911ea828fddc9988526d
Pablo Corral <pcorralr3@gmail.com>
2025-11-04 13:22:25 +0100

    e4

---
+++ PYTHON/Relacion4/e4.py
@@ -0,0 +1,9 @@
+'''
+Escribir un programa que pida al usuario un número entero positivo y muestre por
+pantalla la cuenta atrás desde ese número hasta cero separados por comas.
+'''
+
+entero = int(input("DAME UN NUMERO ENTERO POSITIVO: \n"))
+
+for i in range(entero,0,-1):
+    print(i)
