2724565619aaec87c87f278d2920fe5045763ccf
Pablo Corral <pcorralr3@gmail.com>
2025-11-11 13:46:50 +0100

    e7 a medias

---
+++ PYTHON/Relacion5/e7.py
@@ -0,0 +1,10 @@
+'''
+Ejercicio 7
+Escribir un programa que almacene el abecedario en una lista, elimine de la lista las
+letras que ocupen posiciones múltiplos de 3, y muestre por pantalla la lista
+resultante.
+'''
+abc = []
+for a in range(ord('a'),ord()):
+    abc.append(a)
+    print(a)
